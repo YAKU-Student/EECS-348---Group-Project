ed6c84bb56d339a29dfbfb6ea12beed1a8275c3a
Caden LeCluyse <lecluyse20@gmail.com>
2024-04-27 17:42:55 -0500

    Formatted ast.cpp and ast.h

--- src/ast.h
+++ src/ast.h
@@ -1,33 +1,35 @@
 #ifndef AST_H
 #define AST_H
 
+#include <string>
+
 // Node class which represents a node in a tree
 class Node {
-public:
-  Node(std::string);
-  // Print representation of the node
-  std::string p_key;
-  // Left and right child of the node
-  Node* left_child;
-  Node* right_child;
-  bool is_leaf();
+   public:
+    Node(std::string);
+    // Print representation of the node
+    std::string p_key;
+    // Left and right child of the node
+    Node* left_child;
+    Node* right_child;
+    bool is_leaf();
 };
 
 // Binary tree
 class BT {
-public:
-  // Root of our tree
-  Node* root;
-  BT();
-  ~BT();
-  void print_post_order();
+   public:
+    // Root of our tree
+    Node* root;
+    BT();
+    ~BT();
+    void print_post_order();
 };
 
 // AST definition to represent a boolean expression
 class AST : public BT {
-public:
-  AST();
-  void build_ast_prefix(std::string);
+   public:
+    AST();
+    void build_ast_prefix(std::string);
 };
 
 #endif
