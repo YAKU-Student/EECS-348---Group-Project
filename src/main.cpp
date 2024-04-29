f3928154f237da8bec4edc9ca2945fd37857805d
Caden LeCluyse <lecluyse20@gmail.com>
2024-04-29 16:12:36 -0500

    Added program history
    Added a program history using std::pair
    
    Entering history in the program loop will print all prior expressions

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,57 @@
 // Author: Caden LeCluyse
 
+#include <algorithm>
 #include <iostream>
 #include <string>
+#include <utility>
+#include <vector>
 
 #include "ast.h"
 #include "parser.h"
 
-void evaluate_expression(const std::string& expression) {
+void evaluate_expression(const std::string& expression, std::vector<std::pair<std::string, std::string> >& history) {
     try {
         Parser expression_parser;
         const std::string prefix_expression = expression_parser.create_prefix_expression(expression);
         AST syntax_tree;
         syntax_tree.build_ast_prefix(prefix_expression);
-        const bool result = syntax_tree.evaluate();
+
         std::cout << "Result: ";
-        if (result) {
+        if (syntax_tree.evaluate()) {
             std::cout << "True!\n\n";
+            history.emplace_back(std::make_pair(expression, "True!"));
         } else {
             std::cout << "False!\n\n";
+            history.emplace_back(std::make_pair(expression, "False!"));
         }
     } catch (const std::exception& error) {
         std::cerr << "\nError: " << error.what();
     }
 }
 
-[[nodiscard]] int program_loop() {
+void print_history(const std::vector<std::pair<std::string, std::string> >& history) {
+    std::ranges::for_each(history, [](const auto& expression_result) {
+        std::cout << "Expression: " << expression_result.first << "\nResult: " << expression_result.second << "\n\n";
+    });
+}
+
+[[nodiscard]] int program_loop(auto& program_history) {
     std::string input_expression;
+
     while (true) {
-        std::cout << "Please enter your boolean expression (enter exit, quit, or q to exit the program): ";
+        std::cout << "Please enter your boolean expression, or enter history to see all prior evaluated expressions "
+                  << "(enter exit, quit, or q to exit the program): ";
         // If the input fails for some reason
         if (!std::getline(std::cin, input_expression)) {
             std::cerr << "\nUnknown error ocurred in receiving input. Aborting...\n";
             return 1;
+        } else if (input_expression == "history") {
+            if (program_history.empty()) {
+                std::cerr << "You haven't evaluated any expressions yet!\n";
+            } else {
+                print_history(program_history);
+            }
+            continue;
         } else if (input_expression.empty()) {
             std::cerr << "\nError: Empty expression received!\n";
             continue;
@@ -39,7 +59,8 @@ void evaluate_expression(const std::string& expression) {
             std::cout << "Exiting...\n" << std::endl;
             return 0;
         }
-        evaluate_expression(input_expression);
+
+        evaluate_expression(input_expression, program_history);
     }
 }
 
@@ -56,8 +77,9 @@ int main(int argc, char* const argv[]) {
     }
 
     const std::string expression = argv[1];
+    std::vector<std::pair<std::string, std::string> > program_history;
     if (expression == "-c" || expression == "--continuous") {
-        return program_loop();
+        return program_loop(program_history);
     } else if (expression == "-v" || expression == "--version") {
         std::cout << "Version: " << PROGRAM_VERSION << std::endl;
         return 0;
@@ -65,7 +87,7 @@ int main(int argc, char* const argv[]) {
         std::cerr << "\nError: " << expression << " is an invalid flag.\n";
         return 1;
     }
-    evaluate_expression(expression);
+    evaluate_expression(expression, program_history);
 
     return 0;
 }
